be1703f31eafcf33be3c002d463724c1df20601c
0skar16 <51135536+0skar16@users.noreply.github.com>
2024-09-29 22:05:55 +0200

    make reader read the chunk straight up
    make reader read the chunk straight up, without the chunk reader, this is not an archive, you read the thing once, and all at once

--- src/reader.rs
+++ src/reader.rs
@@ -1,17 +1,14 @@
-use crate::{Chunk, ChunkEntry, DeepslateWorld, CURRENT_VERSION, MAGIC_NUMBER};
+use crate::{Chunk, DeepslateWorld, CURRENT_VERSION, MAGIC_NUMBER};
 use anyhow::{anyhow, bail, Result};
 use byteorder::{LittleEndian, ReadBytesExt};
-use std::{
-    io::{self, Cursor, Read, Seek},
-    ops::Range,
-};
+use std::io::{self, Cursor, Read, Seek};
 
 pub struct DeepslateReader<R> {
     world: DeepslateWorld,
     data_start: u64,
     reader: R,
 }
-impl<'a, R: Read + Seek> DeepslateReader<R> {
+impl<R: Read + Seek> DeepslateReader<R> {
     pub fn new(mut reader: R) -> Result<Self> {
         if reader.read_u64::<LittleEndian>()? != MAGIC_NUMBER {
             bail!("Doesn't start with magic number");
@@ -35,21 +32,38 @@ impl<'a, R: Read + Seek> DeepslateReader<R> {
             data_start,
         })
     }
-    pub fn chunk(&'a mut self, chunk_id: usize) -> Result<ChunkReader<'a, R>> {
-        let mut c = self.data_start;
+    pub fn chunk(&mut self, chunk_id: usize) -> Result<Chunk> {
+        let mut data_start = self.data_start;
         for chunk in &(&self.world.chunks)[0..chunk_id] {
-            c += chunk.len as u64;
+            data_start += chunk.len as u64;
         }
-        let chunk = self
+        let entry = self
             .world
             .chunks
             .get(chunk_id)
             .ok_or_else(|| anyhow!("Couldn't get entry!"))?;
-        Ok(ChunkReader::new(&mut self.reader, c, *chunk))
+        
+        let mut buf = vec![0u8; entry.len];
+        self.reader
+            .seek(io::SeekFrom::Start(data_start))?;
+        self.reader.read_exact(&mut buf)?;
+
+        let buf = match entry.compression {
+            crate::ChunkCompression::None => buf,
+            crate::ChunkCompression::LZ4 => {
+                let mut uncompressed_buf = vec![0u8; entry.original_len];
+                let mut dec = lz4::Decoder::new(Cursor::new(buf))?;
+                dec.read_exact(&mut uncompressed_buf)?;
+                uncompressed_buf
+            }
+            crate::ChunkCompression::Zstd => zstd::decode_all(Cursor::new(buf))?,
+        };
+
+        Ok(bitcode::decode(&buf)?)
     }
-    pub fn chunk_by_pos(&'a mut self, pos: (isize, isize)) -> Result<ChunkReader<'a, R>> {
+    pub fn chunk_by_pos(&mut self, pos: (isize, isize)) -> Result<Chunk> {
         let mut id = 0;
-        for chunk in self.world.chunks.clone() {
+        for chunk in &self.world.chunks {
             if chunk.pos == pos {
                 return self.chunk(id);
             }
@@ -57,49 +71,7 @@ impl<'a, R: Read + Seek> DeepslateReader<R> {
         }
         bail!("Couldn't get entry!");
     }
-    pub fn decoded_chunk_by_pos(&mut self, pos: (isize, isize)) -> Result<Chunk> {
-        let chunk_reader = self.chunk_by_pos(pos)?;
-        chunk_reader.decode()
-    }
     pub fn world(&self) -> DeepslateWorld {
         self.world.clone()
-    }
-}
-
-pub struct ChunkReader<'a, R> {
-    reader: &'a mut R,
-    data_pos_start: u64,
-    entry: ChunkEntry,
-}
-impl<'a, R: Read + Seek> ChunkReader<'a, R> {
-    fn new(reader: &'a mut R, start: u64, entry: ChunkEntry) -> Self {
-        Self {
-            reader,
-            data_pos_start: start,
-            entry,
-        }
-    }
-    pub fn read(self, range: Option<Range<u64>>) -> Result<Vec<u8>> {
-        let range = range.clone().unwrap_or(0..self.entry.len as u64);
-        let rl = range.end - range.start;
-        let mut buf = vec![0u8; rl as usize];
-        self.reader
-            .seek(io::SeekFrom::Start(self.data_pos_start + range.start))?;
-        self.reader.read_exact(&mut buf)?;
-        match self.entry.compression {
-            crate::ChunkCompression::None => Ok(buf),
-            crate::ChunkCompression::LZ4 => {
-                let mut uncompressed_buf = vec![0u8; self.entry.original_len];
-                let mut dec = lz4::Decoder::new(Cursor::new(buf))?;
-                dec.read_exact(&mut uncompressed_buf)?;
-                Ok(uncompressed_buf)
-            }
-            crate::ChunkCompression::Zstd => Ok(zstd::decode_all(Cursor::new(buf))?),
-        }
-    }
-    pub fn decode(self) -> Result<Chunk> {
-        let buf = self.read(None)?;
-        let chunk = bitcode::decode(&buf)?;
-        Ok(chunk)
     }
 }
